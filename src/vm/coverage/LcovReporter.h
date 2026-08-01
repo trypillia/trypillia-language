1c01f414d39e17aed97a5c8f04902b75932e5466
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-01 03:22:10 +0300

    feat: Add LcovReporter to generate standard LCOV coverage files

---
+++ src/vm/coverage/LcovReporter.h
@@ -0,0 +1,12 @@
+#ifndef TRYPILLIA_LCOV_REPORTER_H
+#define TRYPILLIA_LCOV_REPORTER_H
+
+#include "../core/VM.h"
+#include <string>
+
+class LcovReporter {
+  public:
+    static void generateReport(VM *vm, const std::string &outputPath);
+};
+
+#endif // TRYPILLIA_LCOV_REPORTER_H
