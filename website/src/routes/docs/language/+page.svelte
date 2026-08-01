606a29756d8aecab22fdd0c39fa1fbf8c0eecd45
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-01 23:46:15 +0300

    chore: Remove website directory as it has been moved to a separate repository

--- website/src/routes/docs/language/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-	import LanguageOverview from '$lib/components/LanguageOverview.svelte';
-</script>
-
-<LanguageOverview />
--- website/src/routes/docs/stdlib/Json/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-	import ModuleOverview from '$lib/components/ModuleOverview.svelte';
-</script>
-
-<ModuleOverview slug="Json" />
