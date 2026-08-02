6270b3da479e498ead25c48b7f3455b635f8a28b
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-02 16:44:38 +0300

    feat(cli): Add help command API header

---
+++ src/cli/Help.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <ostream>
+#include <string>
+
+namespace cli
+{
+const char *const TRYP_VERSION = "1.0.0";
+const char *const PROG_NAME = "trypillia";
+
+void printGeneralHelp(const std::string &programName, std::ostream &out = std::cout);
+
+bool printCommandHelp(const std::string &programName, const std::string &command, std::ostream &out = std::cout);
+
+void printVersion(const std::string &programName, std::ostream &out = std::cout);
+
+bool printUsageError(const std::string &programName, const std::string &command, std::ostream &out = std::cerr);
+} // namespace cli
