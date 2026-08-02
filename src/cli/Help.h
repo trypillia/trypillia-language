6fa48989e675e8795c3264eb8594a7b8762a1ec6
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-02 17:07:50 +0300

    refactor(cli): Use Version.h constants instead of duplicating in Help.h

--- src/cli/Help.h
+++ src/cli/Help.h
@@ -1,14 +1,12 @@
 #pragma once
 
+#include "Version.h"
 #include <iostream>
 #include <ostream>
 #include <string>
 
 namespace cli
 {
-const char *const TRYP_VERSION = "1.0.0";
-const char *const PROG_NAME = "trypillia";
-
 void printGeneralHelp(const std::string &programName, std::ostream &out = std::cout);
 
 bool printCommandHelp(const std::string &programName, const std::string &command, std::ostream &out = std::cout);
