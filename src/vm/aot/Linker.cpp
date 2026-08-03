02e041ea462f23141f1dc3c908945c30fd6ecf24
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-08-03 17:34:12 +0300

    fix(aot): Reorder static libs so trypillia_core links before trypillia_rt

--- src/vm/aot/Linker.cpp
+++ src/vm/aot/Linker.cpp
@@ -35,7 +35,7 @@ bool Linker::link(const std::string &outPath, const std::vector<std::string> &ob
     {
         cmd << " -L'" << rtDir << "'";
     }
-    cmd << " -ltrypillia_rt -ltrypillia_core -lm -lpthread";
+    cmd << " -ltrypillia_core -ltrypillia_rt -lm -lpthread";
     for (const auto &a : ccArgs)
     {
         cmd << " " << a;
